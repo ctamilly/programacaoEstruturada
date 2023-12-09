144d9e5a7c6a55c0747cb24069652aa84d04bb05
Tamilly <116323270+ctamilly@users.noreply.github.com>
2023-12-09 14:40:32 -0300

    Create ex12.c

---
+++ listaVetores1/ex12.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+
+int main(){
+
+    int numeros[5], maior, menor, soma = 0, media;
+
+    for(int i = 0; i < 0; i++){
+        printf("Digite um numero: ");
+        scanf("\n%d", numeros[i])
+    }
+
+    for(int i = 0; i < 0; i++){
+        if(numeros[i] > maior){
+            maior = numeros[i];
+        }
+
+        if(numeros[i] < menor){
+            maior = numeros[i];
+        }
+     media+= vetor[i];
+ }
+
+   media= media/5;
+
+   for(int i = 0; i<5; i++){
+     printf("\n%d", numeros[i]);
+   }
+  
+   printf("\nO maior valor é: %d",maior);
+   printf("\nO menor valor é: %d",menor);
+   printf("\nA media de valores é: %d",media);
+}
