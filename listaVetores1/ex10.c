c37465aacd1607013d00b80f6fa388f5eb9bd1f0
Tamilly <116323270+ctamilly@users.noreply.github.com>
2023-12-09 14:32:17 -0300

    Create ex10.c

---
+++ listaVetores1/ex10.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+#define TAM 2
+
+int main(){
+    float notas[TAM], media, soma = 0;
+
+    for(int i = 0; i < TAM; i++){
+        printf("\n")
+        scanf("%.2f")
+        soma += notas[i]
+    }
+
+    media = soma/TAM;
+    
+}
